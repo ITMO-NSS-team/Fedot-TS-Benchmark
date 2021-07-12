7d01044719ae3cba369fcce8c2dfb4012adc24eb
J3FALL <crisp.coad@gmail.com>
2021-07-12 17:42:31 +0300

    Implement skip for low scale values

--- analyze/exchange_rate.py
+++ analyze/exchange_rate.py
@@ -14,14 +14,23 @@ def extract_tar(file_in, file_out):
             shutil.copyfileobj(f_in, f_out)
 
 
-def visualize_rates(file_path):
-    exchange = pd.read_csv(file_path, header=None)
-    exchange.columns = EXCHANGE_LABELS
-    plot = sns.lineplot(data=exchange, dashes=False, )
+def visualize_rates(data_frame):
+    plot = sns.lineplot(data=data_frame, dashes=False, )
     plot.set_xlabel('Days')
     plot.set_ylabel('Rate')
     plt.show()
 
 
+def low_scale_only(source_df):
+    resulted = source_df[['New Zealand', 'Singapore']]
+
+    return resulted
+
+
 if __name__ == '__main__':
-    visualize_rates(file_path='../exchange_rate/exchange_rate.txt')
+    exchange = pd.read_csv('../exchange_rate/exchange_rate.txt', header=None)
+    exchange.columns = EXCHANGE_LABELS
+    visualize_rates(exchange)
+
+    low_scale = low_scale_only(exchange)
+    visualize_rates(low_scale)
