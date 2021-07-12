233227c8b70a67634590d246fc17b9d5bced3b1c
J3FALL <crisp.coad@gmail.com>
2021-07-12 17:30:49 +0300

    Add exchange visualization

--- analyze/exchange_rate.py
+++ analyze/exchange_rate.py
@@ -1,5 +1,11 @@
 import gzip
 import shutil
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+EXCHANGE_LABELS = ['Australia', 'British', 'Canada', 'Switzerland',
+                   'China', 'Japan', 'New Zealand', 'Singapore']
 
 
 def extract_tar(file_in, file_out):
@@ -8,6 +14,14 @@ def extract_tar(file_in, file_out):
             shutil.copyfileobj(f_in, f_out)
 
 
+def visualize_rates(file_path):
+    exchange = pd.read_csv(file_path, header=None)
+    exchange.columns = EXCHANGE_LABELS
+    plot = sns.lineplot(data=exchange, dashes=False, )
+    plot.set_xlabel('Days')
+    plot.set_ylabel('Rate')
+    plt.show()
+
+
 if __name__ == '__main__':
-    extract_tar('../exchange_rate/exchange_rate.txt.gz',
-                '../exchange_rate/exchange_rate.txt')
+    visualize_rates(file_path='../exchange_rate/exchange_rate.txt')
