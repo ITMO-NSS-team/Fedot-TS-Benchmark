d8dc49b7daa0bbe4bef93985becdf97d086c98b1
ChrisLisbon <yulashka.htm@yandex.ru>
2021-09-07 11:50:26 +0300

    launcher class (only for fedot) implemented

--- fedot_benchmark_shall/launcher.py
+++ fedot_benchmark_shall/launcher.py
@@ -3,30 +3,27 @@ import os
 
 import pandas as pd
 from fedot.api.main import Fedot
-from fedot.core.repository.tasks import Task, TaskTypesEnum, TsForecastingParams
-from fedot.core.data.data import InputData
-from fedot.core.repository.dataset_types import DataTypesEnum
+from fedot.core.repository.tasks import TsForecastingParams
 
 
 class Launcher:
-    def __init__(self, framework_name: str, data: pd.DataFrame, horizon_size: int, horizon_step: int = 10,
+    def __init__(self, framework_name: str, features, target: str, horizon_size: int, horizon_step: int = 10,
                  timeout: [float, int] = 2):
         self.framework_name = framework_name
         self.horizons = np.arange(horizon_step, horizon_size, horizon_step)
         self.timeout = timeout
 
-        self.data = data
-        self.features = None
-        self.target = None
+        self.features = features
+        self.target = target
 
     def run_forecasting(self, predict_outfolder, structure_outfolder):
         if self.framework_name == 'fedot':
             for horizon in self.horizons:
-                self._form_input_data(horizon)
                 self._run_fedot(horizon, predict_outfolder, structure_outfolder)
 
         else:
             raise NotImplementedError
+
 
     def _run_fedot(self, forecast_len, predict_outfolder, structure_outfolder):
         model = Fedot(problem='ts_forecasting',
@@ -34,7 +31,8 @@ class Launcher:
                       timeout=self.timeout,
                       composer_params={'with_tuning': True,
                                        'cv_folds': 2,
-                                       'validation_blocks': 2})
+                                       'validation_blocks': 2,
+                                       'history_folder': 'output_structure'})
         pipeline = model.fit(features=self.features, target=self.target)
         image_name = f'{self.target}_fedot_forcast{forecast_len}_time{self.timeout}.png'
         pipeline_name = f'{self.target}_fedot_forcast{forecast_len}_time{self.timeout}.json'
@@ -44,22 +42,3 @@ class Launcher:
         df_name = f'{self.target}_fedot_forcast{forecast_len}_time{self.timeout}.csv'
         forecast = model.predict(features=self.features)
         pd.DataFrame({'Prediction': forecast}).to_csv(os.path.join(predict_outfolder, df_name), index=False)
-
-    def _form_input_data(self, len_forecast):
-        train_part = self.data
-        task = Task(TaskTypesEnum.ts_forecasting,
-                    TsForecastingParams(forecast_length=len_forecast))
-
-        train_input = InputData(idx=np.arange(0, len(train_part)),
-                                features=train_part,
-                                target=train_part,
-                                task=task,
-                                data_type=DataTypesEnum.ts)
-        self.features = train_input
-
-        predict_input = InputData(idx=np.arange(len(train_part)-len_forecast, len(train_part)),
-                                  features=train_part,
-                                  target=None,
-                                  task=task,
-                                  data_type=DataTypesEnum.ts)
-        self.target = predict_input
--- fedot_benchmark_shall/launcher_test.py
+++ fedot_benchmark_shall/launcher_test.py
@@ -1,13 +1,10 @@
-import pandas as pd
-import numpy as np
+import os
 from launcher import Launcher
 
 
-short_df = pd.read_csv('../time_series_case_1/data/ts_short.csv')
+for file in os.listdir('input_datasets'):
+    data_path = os.path.join('input_datasets', file)
 
-for label in short_df['series_id'].unique():
-    df = short_df[short_df['series_id'] == label]
-
-    launcher = Launcher(framework_name='fedot', data=df,  horizon_size=100, horizon_step=10,
+    launcher = Launcher(framework_name='fedot', features=data_path, target='value', horizon_size=100, horizon_step=10,
                         timeout=2)
     launcher.run_forecasting(structure_outfolder='output_structure', predict_outfolder='output_predictions')
